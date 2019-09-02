a2bc0ce4c138ccb19cab84407402cef2281985e7
Javier <javier.cestau@telesoftmobile.com>
2019-09-02 23:50:18 +0200

    moving setTimeout to addmessage function

--- src/components/helpers/alert_messages/index.jsx
+++ src/components/helpers/alert_messages/index.jsx
@@ -3,8 +3,6 @@ import Toast from 'components/base/toast';
 
 import './alert_messages.sass'
 
-const alreadySet = []
-
 export const AlertMessagesContext = React.createContext();
 
 export class AlertMessages extends React.Component {
@@ -13,6 +11,16 @@ export class AlertMessages extends React.Component {
   }
 
   addAlertMessages = (newMessages) => {
+    newMessages.forEach((message, i) => {
+      const transitionTimeMs = 3000 + (i * 1000)
+      setTimeout(() => {
+        this.setState((prevState) => {
+          prevState.alertMessages.shift()
+          return ({
+            alertMessages: prevState.alertMessages
+        })})
+        }, transitionTimeMs)
+    })
     this.setState({
       alertMessages: this.state.alertMessages.concat(newMessages)
     })
@@ -28,18 +36,9 @@ export class AlertMessages extends React.Component {
         }
         <div className="toast-container">
         {
-          this.state.alertMessages.map((alertMessage, i) => {
-            const transitionTimeMs = 3000 + (i * 1000)
-            if (alreadySet.indexOf(i) === -1) {
-              alreadySet.push(i)
-              setTimeout(() => {
-                this.setState((prevState) => ({
-                  alertMessages: prevState.alertMessages.splice(0, prevState.alertMessages.length - 1)
-                }))
-              }, transitionTimeMs)
-            }
-            return <Toast key={i} {...alertMessage}/>
-          })
+          this.state.alertMessages.map((alertMessage, i) => (
+            <Toast key={i} {...alertMessage}/>
+          ))
         }
         </div>
       </AlertMessagesContext.Provider>
