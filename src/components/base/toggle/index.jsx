267e0b20a11efccb43e58bba8ccc60e856696c63
an vo <>
2020-06-19 15:24:01 +0700

    add label for Toggle Component

--- src/components/base/toggle/index.jsx
+++ src/components/base/toggle/index.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './toggle.module.sass';
 
-const Toggle = ({ value, onChange, positiveText = 'YES', negativeText = 'NO' }) => {
+const Toggle = ({ value, onChange, label, positiveText = 'YES', negativeText = 'NO' }) => {
   return (
-    <div
-      className={`${styles.toggle} ${value ? styles.toggleOn : ''}`}
-      onClick={() => onChange(!value)}
-    >
-      <div>
-        <span>
-          <span>{positiveText}</span>
-        </span>
-        <span>
-          {negativeText}
-        </span>
+    <div className="d-flex align-items-center">
+      <div
+        className={`${styles.toggle} ${value ? styles.toggleOn : ''}`}
+        onClick={() => onChange(!value)}
+      >
+        <div>
+          <span>
+            <span>{positiveText}</span>
+          </span>
+          <span>
+            {negativeText}
+          </span>
+        </div>
       </div>
+      {!!label &&
+        <span className="general-text-2">{label}</span>
+      }
     </div>
   );
 };
@@ -23,6 +28,7 @@ const Toggle = ({ value, onChange, positiveText = 'YES', negativeText = 'NO' }) 
 Toggle.propTypes = {
   value: PropTypes.bool.isRequired,
   onChange: PropTypes.func.isRequired,
+  label: PropTypes.string,
   positiveText: PropTypes.string,
   negativeText: PropTypes.string
 };
