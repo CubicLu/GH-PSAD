2f15942840237a34ebcc637c0a475e7444f2c387
an vo <>
2020-07-12 21:54:53 +0700

    PSAD-1341

--- src/components/base/table/index.jsx
+++ src/components/base/table/index.jsx
@@ -151,7 +151,7 @@ export class IndexTable extends React.Component {
         </table>
         <Pagination
           {...this.props}
-          className="py-4"
+          className="my-4"
           query={query}
           stopFetchingPagination={this.stopFetchingActionTable}
           startFetchingPagination={this.startFetchingActionTable}
