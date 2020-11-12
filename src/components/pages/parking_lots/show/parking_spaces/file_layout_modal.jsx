918018e32811cbaa1ab40834d5906a0ed4a1ee16
An Vo <anvotrinh@gmail.com>
2020-11-12 17:54:20 +0700

    PSAD-1793

--- src/components/pages/parking_lots/show/parking_spaces/file_layout_modal.jsx
+++ src/components/pages/parking_lots/show/parking_spaces/file_layout_modal.jsx
@@ -4,71 +4,88 @@ import ReactFileReader from 'react-file-reader';
 import { Modal, ModalHeader, ModalBody, Button, Input, Label, Col, Row } from 'reactstrap';
 import TooltipInfo from 'components/helpers/tooltip_info';
 
-const FileLayoutModal = (props) => {
-  const { saveParkingPlanFile, isOpen, toggleModal, defaultName, defaultURL } = props;
-  const [file, setFile] = useState(null)
-  const [fileName, setFileName] = useState(defaultName)
-  const [fileURL, setFileURL] = useState(defaultName)
+const FileLayoutModal = ({ saveParkingPlanFile, isOpen, toggleModal, defaultName, defaultURL }) => {
+  const [file, setFile] = useState(null);
+  const [name, setName] = useState(defaultName);
+  const [fileURL, setFileURL] = useState(defaultURL);
+  const [displayError, setDisplayError] = useState(false);
 
   useEffect(() => {
-    setFileName(defaultName)
-    setFileURL(defaultURL)
+    setName(defaultName);
+    setFileURL(defaultURL);
   }, [defaultName, defaultURL]);
 
+  const handleSave = () => {
+    if (!name || !(file || fileURL)) {
+      setDisplayError(true);
+      return;
+    }
+    saveParkingPlanFile(file, name);
+    setName('');
+    setFile(null);
+    toggleModal();
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value.trim());
+  };
+
+  const showNameError = displayError && !name;
+  const showFileError = displayError && !file && !fileURL;
   return (
     <Modal centered={true} isOpen={isOpen} toggle={toggleModal} size="md">
       <ModalHeader className="justify-content-center border-0 h2-title">Add New Layout</ModalHeader>
-      <ModalBody>
-        <Row className="justify-content-center">
-          <Col sm={12} md={9}>
-            <Col>
-              <Col className="pr-0" sm={12}>
-                <Label className="general-text-1">
-                  <span className={`mr-1 text-primary`}>*</span>
-                  Layout Name
-                </Label>
-                <Input onChange={(e) => setFileName(e.target.value)} value={fileName} placeholder="Enter Name Here" />
-              </Col>
-            </Col>
-            <Col className="d-flex justify-content-center align-items-center mt-3" sm={12}>
-              <Col className="pr-0" sm={9}>
-                  <Label className="general-text-1">
-                    <span className={`mr-1 text-primary`}>*</span>
-                    Layout Image
-                  </Label>
-                  <Input placeholder="Image URL" readOnly value={fileURL}/>
-                  <span className="general-text-3">
-                    Format for image:  Jpeg, Png
-                  </span>
-                  <TooltipInfo className="ml-2" text="This is the layout image" target="picture" />
-              </Col>
-              <Col className="pl-1 pt-1" sm={3}>
-                <ReactFileReader
-                  base64={true}
-                  handleFiles={setFile}
-                >
-                  <Button>Browse</Button>
-                </ReactFileReader>
-
-              </Col>
-            </Col>
-            <Col className="d-flex justify-content-center align-items-center mt-4">
-              <Button color="danger" className="px-5 py-2 mx-2" onClick={toggleModal}>Cancel</Button>
-              <Button color="success" className="px-5 py-2 mx-2 my-4" onClick={() => {
-                saveParkingPlanFile(file, fileName);
-                setFileName("");
-                setFile(null)
-                toggleModal();
-              }}>Save</Button>
-            </Col>
+      <ModalBody className="px-4">
+        <Label className="general-text-1 d-block">
+          <span className="mr-1 text-primary">*</span>
+          Layout Name
+        </Label>
+        <Input
+          id='layoutNameInput'
+          className={`form-control ${showNameError ? 'input-error' : ''}`}
+          onChange={handleNameChange}
+          value={name}
+          placeholder="Enter Name Here"
+        />
+        {showNameError &&
+          <div className='general-error general-text-1 mt-1'>Layout Name is required</div>
+        }
+        <Label className="general-text-1 mt-3 d-block">
+          <span className="mr-1 text-primary">*</span>
+          Layout Image
+        </Label>
+        <Row>
+          <Col sm={9}>
+            <Input
+              readOnly
+              className={`form-control ${showFileError ? 'input-error' : ''}`}
+              value={fileURL || (file ? file.fileList[0].name : 'Image URL')}
+            />
+            {showFileError &&
+              <div className='general-error general-text-1 d-block mt-1'>Layout Image is required</div>
+            }
+            <div className="general-text-3 mt-1">
+              Format for image:  Jpeg, Png
+              <TooltipInfo className="ml-2" text="This is the layout image" target="picture" />
+            </div>
+          </Col>
+          <Col className="pl-1" sm={3}>
+            <ReactFileReader
+              base64={true}
+              handleFiles={setFile}
+            >
+              <Button className="w-100">Browse</Button>
+            </ReactFileReader>
           </Col>
         </Row>
+        <div className="d-flex justify-content-center mt-4 my-4">
+          <Button color="danger" className="px-5 py-2 mx-2" onClick={toggleModal}>Cancel</Button>
+          <Button color="success" className="px-5 py-2 mx-2" onClick={handleSave}>Save</Button>
+        </div>
       </ModalBody>
-
     </Modal>
-
-  )
-}
+  );
+};
 
 FileLayoutModal.propTypes = {
   saveParkingPlanFile: PropTypes.func.isRequired,
@@ -76,6 +93,6 @@ FileLayoutModal.propTypes = {
   toggleModal: PropTypes.func.isRequired,
   defaultName: PropTypes.string.isRequired,
   defaultURL: PropTypes.string.isRequired
-}
+};
 
 export default FileLayoutModal;
